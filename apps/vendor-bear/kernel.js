cf2fb77854e581e45c9313847f0b7620189173aa
mehgokalp <mehgokalp98@gmail.com>
2020-01-04 12:48:33 +0300

    - Test framework jest initialised

---
+++ apps/vendor-bear/kernel.js
@@ -0,0 +1,4 @@
+const loader = require('./loader');
+loader('models');
+loader('validators');
+require('dotenv').config();
---
+++ apps/vendor-bear/tests/sum.test.js
@@ -0,0 +1,5 @@
+require('../kernel');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
